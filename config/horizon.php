74d1f9cd6338372e783e23651956bfa3c15b1b01
Robert <hb7dXEXggo6)Qq%EGs>
2022-10-09 13:22:29 -0400

    Reset home page and navigation

--- config/horizon.php
+++ config/horizon.php
@@ -187,7 +187,13 @@ return [
                 'balanceCooldown' => 3,
             ],
         ],
-
+        'development' => [
+            'supervisor-1' => [
+                'maxProcesses' => 10,
+                'balanceMaxShift' => 1,
+                'balanceCooldown' => 3,
+            ],
+        ],
         'local' => [
             'supervisor-1' => [
                 'maxProcesses' => 3,
