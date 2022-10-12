c8f6c8a20f50a5b5275a641b49cd8baf0f8b650b
Robert <hb7dXEXggo6)Qq%EGs>
2022-10-12 11:53:15 -0400

    Remove timezone post

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,4 +1,5 @@
 import _ from 'lodash';
+
 window._ = _;
 
 /**
@@ -8,9 +9,17 @@ window._ = _;
  */
 
 import axios from 'axios';
+
 window.axios = axios;
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+
+/*
+const tz = Intl.DateTimeFormat().resolvedOptions().timeZone;
+axios.post('/timezone', {value: tz})
+    .then((response) => {
+    });
+*/
 
 /**
  * Echo exposes an expressive API for subscribing to channels and listening
