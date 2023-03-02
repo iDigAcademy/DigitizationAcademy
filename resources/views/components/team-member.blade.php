2a0f9dca4185fa401185692248e31239a5af0be2
Robert <hb7dXEXggo6)Qq%EGs>
2023-03-02 16:36:40 -0500

    Hide email and twitter handle on index, remove required.

--- resources/views/components/team-member.blade.php
+++ resources/views/components/team-member.blade.php
@@ -1,4 +1,6 @@
-<div {{ $attributes->merge(['class' => 'process-work'])->merge(['data-sal' => 'slide-right']) }} data-sal-duration="1000" data-sal-delay="100">
+<div
+    {{ $attributes->merge(['class' => 'process-work'])->merge(['data-sal' => 'slide-right']) }} data-sal-duration="1000"
+    data-sal-delay="100">
     <div class="thumbnail paralax-image">
         <img src="{{ $team->present()->team_image }}" alt="Thumbnail">
     </div>
@@ -6,7 +8,9 @@
         <h2 class="title text-light mb-1">{{ $team->name }}</h2>
         <span class="subtitle">{{ $team->title }}</span>
         <ul class="social-share list-unstyled d-flex mb-3">
-            <li><a href="mailto:{{ $team->email }}"><i class="fa fa-envelope"></i></a></li>
+            @isset($team->email)
+                <li><a href="mailto:{{ $team->email }}"><i class="fa fa-envelope"></i></a></li>
+            @endisset
             @isset($team->twitter_handle)
                 <li><a href="//twitter.com/{{ $team->twitter_handle }}"><i class="fab fa-twitter"></i></a></li>
             @endisset
