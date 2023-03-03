3f3f3d8bb8248cf1bd493c1934b13fac8b8323d3
Robert <hb7dXEXggo6)Qq%EGs>
2023-03-03 09:23:11 -0500

    Push change for Google translate for testing.

--- resources/views/components/header-navbar.blade.php
+++ resources/views/components/header-navbar.blade.php
@@ -46,6 +46,16 @@
                                     @endforeach
                                 </div>
                             </li>
+                            <li>
+                                <div id="google_translate_element"></div>
+                                <script type="text/javascript">
+                                    function googleTranslateElementInit() {
+                                        new google.translate.TranslateElement({pageLanguage: 'en', layout: google.translate.TranslateElement.InlineLayout.SIMPLE}, 'google_translate_element');
+                                    }
+                                </script>
+
+                                <script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
+                            </li>
                         </ul>
                     </nav>
                 </div>
