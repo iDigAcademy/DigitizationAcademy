8a92ba1967d52158e06e0dc7e67c77259a3948ba
rbruhn <bruhnrp@gmail.com>
2025-10-11 13:27:04 -0400

    Update composer to use latest packages and fixed error with Nova and AutoCache.

--- config/app.php
+++ config/app.php
@@ -199,6 +199,7 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
+        App\Providers\AutoCacheAdminServiceProvider::class,
 
         /*
          * Package Service Providers...
